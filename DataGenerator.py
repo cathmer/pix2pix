0b5d356dc31faf44ae948d823778df78c72d6f0c
Luc <luc@lenferink.tech>
2020-03-17 15:26:17 +0100

    Save temp work

---
+++ DataGenerator.py
@@ -0,0 +1,32 @@
+import os
+from torchvision import transforms
+
+#
+# print("t is: ", t.size())
+#
+# im = transforms.ToPILImage()(t).convert("RGB")
+# display(im)
+# print(im)
+# print(im.size)
+
+
+
+
+#
+# def get_train_data():
+#     base_path = os.path.abspath(".")
+#     path = os.path.join(base_path, 'dataset', 'cityscapes', 'train')
+#
+#
+#
+#     print(base_path)
+#
+#     path = r'D:\Code\ImageToImage\pix2pix\dataset\cityscapes\train'
+#
+#
+#
+# get_train_data()
+#
+# # def create_generator(images):
+# #     for i in images:
+# #         yield i
--- Train.py
+++ Train.py
@@ -1,6 +1,17 @@
+from Pix2PixOptimizer import Pix2PixOptimizer
+
+
+
 
 
 if __name__ == '__main__':
 
-    images = get_images()
+    use_GAN = True
+    is_conditional = True
+    has_L1 = True
 
+    model = Pix2PixOptimizer()
+    # images_dataset = get_images()
+    #
+    # for images in images_dataset:
+    #     yield images
