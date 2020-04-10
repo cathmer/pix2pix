1ce208963162c60ada9bb9cfb3dc5c320cd7cb83
Casper Athmer <casperathmer92@gmail.com>
2020-04-10 11:31:02 +0200

    Set label sizes 1024

--- dataset/cityscapes/resize_labels.sh
+++ dataset/cityscapes/resize_labels.sh
@@ -5,5 +5,5 @@ cd original_labels
 for filename in *.png; do
 	echo $filename
 	# Remove \! if you want to keep the original ratio of the label image
-	convert -resize 256x256\! $filename ../resized_labels/$filename
+	convert -resize 1024x512 $filename ../resized_labels/$filename
 done
